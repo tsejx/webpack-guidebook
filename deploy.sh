2e7cc5e76eceab07e70a4da596d8f4971641b10b
tsejx <kufong1994@gmail.com>
2020-01-17 22:06:09 +0800

    :recycle: refactor: deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+yarn run build
+cd .vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:tsejx/webpack-guidebook.git master:gh-pages
+
+cd -
