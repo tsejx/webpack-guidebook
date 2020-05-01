f449758099a591224950560818968ce51e041a88
tsejx <tsejx@foxmail.com>
2020-05-01 20:09:28 +0800

    :sparkles: feat: add webpack4-ssr-demo demo

---
+++ example/webpack4-ssr-demo/src/index.js
@@ -0,0 +1,10 @@
+const React = require('react');
+const css = require('./index.css');
+
+class App extends React.Component {
+  render() {
+    return <div className="container">Hello world!</div>;
+  }
+}
+
+module.exports = <App />;
---
+++ example/webpack4-ssr-demo/webpack.ssr.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      inject: true,
+      minify: {
+        // SSR 要替换构建产物 HTML 中的注释，所以这个要打开
+        removeComments: false,
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+    }),
+  ],
+};
