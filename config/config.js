11db5fd6f121523f071fcde995321877248248ac
tsejx <tsejx@foxmail.com>
2020-05-10 15:00:25 +0800

    :wrench: config: update build configuration

--- config/config.js
+++ config/config.js
@@ -1,17 +1,18 @@
 export default {
-    mode: 'site',
-    title: 'Webpack Guidebook',
-    description: 'Webpack 完全知识体系',
-    base: '/webpack-guidebook/',
-    publicPath: '/webpack-guidebook/',
-    favicon: './favicon.ico',
-    logo: './favicon.svg',
-    exportStatic: {},
-    navs: [
-      null,
-      {
-        title: 'Github',
-        path: 'https://github.com/tsejx/webpack-guidebook',
-      },
-    ],
-  };
+  mode: 'site',
+  hash: true,
+  title: 'Webpack Guidebook',
+  description: 'Webpack 完全知识体系',
+  base: '/webpack-guidebook/',
+  publicPath: '/webpack-guidebook/',
+  favicon: './favicon.ico',
+  logo: 'http://img.mrsingsing.com/webpack-guidebook-favicon.svg',
+  exportStatic: {},
+  navs: [
+    null,
+    {
+      title: 'Github',
+      path: 'https://github.com/tsejx/webpack-guidebook',
+    },
+  ]
+};
