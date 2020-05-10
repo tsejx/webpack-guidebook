93ccf17f5c5ea60bfccedeb9fa11de8e581eb162
tsejx <tsejx@foxmail.com>
2020-05-10 13:55:05 +0800

    :memo: example: add parcel-demo

---
+++ example/parcel-demo/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.less';
+
+const App = () => <h1>Hello world!</h1>;
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root);
